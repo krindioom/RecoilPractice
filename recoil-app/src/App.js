11456da7442164f3cb26bac1f898598f72f64248
krindioom <47291437+krindioom@users.noreply.github.com>
2024-01-10 18:56:35 +0700

    Добавил стартовые библиотеки

--- recoil-app/src/App.js
+++ recoil-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
+
 
 function App() {
   return (
-    <>
-      
-    </>
+    <RecoilRoot>
+      sx
+    </RecoilRoot>
   );
 }
 
