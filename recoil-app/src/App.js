a56d111ea22b172a42ce8067a0a7e9538afd4a4e
krindioom <47291437+krindioom@users.noreply.github.com>
2024-01-16 03:22:28 +0700

    ничего не делал

--- recoil-app/src/App.js
+++ recoil-app/src/App.js
@@ -1,17 +1,13 @@
-//import logo from './logo.svg';
-import "./App.css";
-import { RecoilRoot } from "recoil";
-import { Route, Routes } from "react-router-dom";
-import Main from "./mainPage/Main";
+  //import logo from './logo.svg';
+  import "./App.css";
+  import { RecoilRoot } from "recoil";
+  import { Route, Routes } from "react-router-dom";
+  import Main from "./mainPage/Main";
 
-function App() {
-    return (
-        <RecoilRoot>
-            <Routes>
-                <Route path="/" element={<Main />} />
-            </Routes>
-        </RecoilRoot>
-    );
-}
+  function App() {
+      return (
+        <></>
+      );
+  }
 
-export default App;
+  export default App;
